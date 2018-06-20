eb9f2072e7237e88c6c990612f4095a861fa26f9
Mousa Ghannam <Mousa.Ghannam1@gmail.com>
2018-06-20 02:17:13 -0400

    Add project files.

---
+++ CampbellBonuses/CampbellBonusesDataSet.vb
@@ -0,0 +1,19 @@
+﻿Partial Class CampbellBonusesDataSet
+    Partial Public Class Employee_BonusDataTable
+        Private Sub Employee_BonusDataTable_ColumnChanging(sender As Object, e As DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            If (e.Column.ColumnName = Me.EIDColumn.ColumnName) Then
+                'Add user code here
+            End If
+
+        End Sub
+
+    End Class
+End Class
+
+Namespace CampbellBonusesDataSetTableAdapters
+    Partial Public Class Employee_BonusTableAdapter
+    End Class
+
+    Partial Public Class IncentivesInformationTableAdapter
+    End Class
+End Namespace
